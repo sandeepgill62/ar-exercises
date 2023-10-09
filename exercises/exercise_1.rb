b4ea123268e4245f5c970b212af376ef69578fc1
Sandeep Gill <sandeepgill62@gmail.com>
2023-10-09 01:31:31 +0000

    exercise 1. create three stores. count stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,14 +7,4 @@ Store.create(:name => 'Burnaby', :annual_revenue => 300000, :mens_apparel => tru
 Store.create(:name => 'Richmond', :annual_revenue => 1260000, :mens_apparel => false, :womens_apparel => true)
 Store.create(:name => 'Gastown', :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false)
 
-# - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-# - Richmond (annual_revenue of 1260000 carries women's apparel only)
-# - Gastown (annual_revenue of 190000 carries men's apparel only)
-
-# create_table :stores do |t|
-#   t.column :name, :string
-#   t.column :annual_revenue, :integer
-#   t.column :mens_apparel, :boolean
-#   t.column :womens_apparel, :boolean
-#   t.timestamps null: false
-# end
+puts "Total store created: " + (Store.count).to_s
